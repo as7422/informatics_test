5319d69fa2f6dd2847a89f1b2b7a45d1ef9b7262
as7422 <as7422@columbia.edu>
2025-12-13 15:32:26 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const map = L.map('map').setView([40.7128, -74.0060], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+fetch('data/city_nta.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        const name = feature.properties.NTAName || 'NTA';
+        layer.bindPopup(`<strong>${name}</strong>`);
+      },
+      style: {
+        fillColor: '#3182bd',
+        weight: 0.5,
+        color: '#333',
+        fillOpacity: 0.6
+      }
+    }).addTo(map);
+  });
+#map {
+  height: 80vh;
+  width: 100%;
+}
